1db58177b77754bb2b14147769bc2de271bb204c
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-03 01:41:32 +0200

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    str1 = "FizzBuzz"
+    str2 = "Fizz"
+    str3 = "Buzz"
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{}".format(str1), end=" ")
+        elif i % 3 == 0:
+            print("{}".format(str2), end=" ")
+        elif i % 5 == 0:
+            print("{}".format(str3), end=" ")
+        else:
+            print("{}".format(i), end=" ")
+    print("$", end="")
