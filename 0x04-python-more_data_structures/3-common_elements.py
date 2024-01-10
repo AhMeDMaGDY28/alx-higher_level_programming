e0b88c8142b63d12b2ed5bee9fd44f6cb42d4a74
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-10 17:09:53 +0200

    fix task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    holder = ""
+    holder = []
     for i in set_1:
         for x in set_2:
             if i == x:
-                holder = i
+                holder.append(i)
     return (holder)
