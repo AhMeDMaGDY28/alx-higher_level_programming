5edcef7abda0ffcb72f7d0d4482802d26c19261d
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-24 23:03:14 +0200

    task 4 fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new[i] = div
+            new.append(div)
     return new
