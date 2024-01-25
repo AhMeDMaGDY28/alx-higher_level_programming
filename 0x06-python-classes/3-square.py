59a8111287eb461e65feb2d8447e2aa7eabb9b67
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-25 03:02:44 +0200

    task 3 fix 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = __size
+        self.__size = size
     """this function to measure the area of the square."""
     def area(self):
         return self.__size**2
