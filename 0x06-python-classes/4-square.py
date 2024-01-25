30471ec8abfb125fe4a17699b4fbf000a7e8f02d
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-01-25 03:21:23 +0200

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""this is docemntion for square"""
+
+
+class Square:
+    """square size control"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    """this function to measure the area of the square."""
+    def area(self):
+        return self.__size**2
+
+    """this is size function"""
+    @property
+    def size(self):
+        return self.__size
+    """i dont know what i call tis """
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
