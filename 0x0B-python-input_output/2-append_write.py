4860d208668dc277e241eda3d42102dde6a53d17
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-07 01:25:05 +0200

    T2 fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This module defines a file-abending function."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Abends a string to a UTF8 text file"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(f"{text}")
