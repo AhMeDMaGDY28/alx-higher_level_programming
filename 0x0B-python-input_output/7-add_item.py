cb046e5bc1cff7e193f859c51b7e60b1dc53c73f
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-07 02:09:10 +0200

    T7 fix 1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """ha script that adds all arguments to a Python list
 and then save them to a file:"""
 import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
@@ -11,5 +12,5 @@ try:
     list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     list = []
-    list.extend(sys.argv[1:])
-    save_to_json_file(list, "add_item.json")
+list.extend(sys.argv[1:])
+save_to_json_file(list, "add_item.json")
