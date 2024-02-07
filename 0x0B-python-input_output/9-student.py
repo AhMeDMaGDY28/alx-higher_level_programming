1c828c2ef9ff34e83734a4958bbc4c2defd46a69
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-07 02:19:12 +0200

    T9wq

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """a class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """init a new student"""
+        slef.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return the dict"""
+        return self.__dict__
