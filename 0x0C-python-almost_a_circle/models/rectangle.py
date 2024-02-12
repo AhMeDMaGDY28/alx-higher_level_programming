a24f059d8e5ed962427e430522bdbc60893cfd68
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-12 21:29:12 +0200

    fix 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,13 +7,13 @@ from models.base import Base
 
 class Rectangle(Base):
     """class Rectangle implements Base"""
-    def _init_(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle."""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super()._init_(id)
 
     @property
     def width(self):
