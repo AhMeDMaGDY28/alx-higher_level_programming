7b673ea59aa2381373455a7cb4525eafda603b7f
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-02-12 22:34:57 +0200

    fix 12

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,53 +17,73 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON"""
-        if list_dictionaries is None or list_dictionaries == {}:
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save the file"""
-        file_name = cls.__name__ + ".json"
-        dict_list = []
-        if list_objs is not None:
-            for instance in list_objs:
-                dict_list.append(instance.to_dictionary())
-        with open(file_name, 'w', encoding='utf-8') as file_a:
-            file_a.write(Base.to_json_string(dict_list))
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
-        """loads an object form JSON"""
-        if json_string is None:
+        """Return the deserialization of a JSON string.
+
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        from models.rectangle import Rectangle
-        from models.square import Square
+        """Return a class instantied from a dictionary of attributes.
 
-        if cls is Rectangle:
-            obj = Rectangle(2, 2)
-        elif cls is Square:
-            obj = Square(4)
-        else:
-            obj = None
-        obj.update(**dictionary)
-        return obj
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
 
     @classmethod
     def load_from_file(cls):
-        """loads JSON from file and convert it to object"""
-        from models.rectangle import Rectangle
-        from models.square import Square
-        file_name = cls.__name__ + ".json"
-        with open(file_name, encoding='utf-8') as file_a:
-            if cls is Rectangle:
-                return [Rectangle.create(**dictionary) for dictionary
-                        in Rectangle.from_json_string(file_a.read())]
-            if cls is Square:
-                return [Square.create(**dictionary) for dictionary
-                        in Square.from_json_string(file_a.read())]
+        """Return a list of classes instantiated from a file of JSON strings.
+
+        Reads from `<cls.__name__>.json`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
