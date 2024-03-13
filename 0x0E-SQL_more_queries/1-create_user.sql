8f0fb2e6c657da0f50cee7b33b3cbf5528e9a99a
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-14 01:35:28 +0200

    T1 fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,2 @@
 -- a script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
