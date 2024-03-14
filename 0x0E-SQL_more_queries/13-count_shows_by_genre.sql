d9374de6d44624ec56cf93ec5fb075e340fa306b
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-14 02:38:19 +0200

    testing

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT tv_show_genres.genre_id AS genre,
-       COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-GROUP BY tv_show_genres.genre_id
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS number_of_shows 
+FROM tv_show_genres 
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+GROUP BY tv_genres.name 
 ORDER BY number_of_shows DESC;
-
