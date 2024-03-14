a9fb6c3c63eee41685ca86fc25625c94a42c0e31
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-03-14 03:15:35 +0200

    T12 بهء

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,6 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY rating DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
+WHERE tv_show_genres.genre_id is NULL 
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
