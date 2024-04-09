936ee2bdfe1ecaa730e35ec3e9f96fad89723248
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-09 04:14:26 +0200

    T2 fix 4

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// Prints a message depending on number of arguments passed
+// Print a message depending on number of arguments passed
 
-const argv = process.argv;
-if (argv.length === 2) {
-  console.log("No argument");
-} else if (argv.length === 3) {
-  console.log("Argument found");
+const args = process.argv;
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
