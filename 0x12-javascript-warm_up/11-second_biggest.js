a080123ec585ecf4e5ee40f449147809d28d53b9
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-09 04:39:54 +0200

    T11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
-// Search the second biggest integer in the list of arguments
+let first = -Infinity;
+let second = -Infinity;
+let number;
+const argv = process.argv;
 
-const args = process.argv;
-
-if (isNaN(args[2])) {
-  console.log('0');
-} else if (args.length === 3) {
-  console.log('0');
-} else {
-  let first = parseInt(args[2], 10);
-  let second = parseInt(args[3], 10);
-  for (let i = 2; i < args.length; i++) {
-    if (parseInt(args[i], 10) > first) {
-      second = first;
-      first = parseInt(args[i], 10);
-    }
-    if (parseInt(args[i], 10) > second && parseInt(args[i], 10) < first) {
-      second = parseInt(args[i], 10);
-    }
+for (let i = 2; argv[i]; i++) {
+  number = Math.floor(+argv[i]);
+  if (number > first) {
+    [first, second] = [number, first];
+  } else if (number > second) {
+    second = number;
   }
-  console.log(second);
 }
+console.log(second !== -Infinity ? second : 0);
