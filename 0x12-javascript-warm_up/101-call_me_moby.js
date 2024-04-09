7f737440612c254f329ea0aa35f4dd09a8f2cc09
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-09 04:45:29 +0200

    T12

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function callMeMoby (x, theFunction) {
-	while (x-- > 0) {
-	  theFunction();
-	}
-   }
-   module.exports = {
-	callMeMoby
-   };
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-function addMeMaybe (x, theFunction) {
-	theFunction(++x);
-   }
-   module.exports = {
-	addMeMaybe
-   };
