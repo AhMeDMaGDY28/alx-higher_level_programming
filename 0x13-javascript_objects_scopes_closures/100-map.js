9164cd540a2a901e1c3e61b3cd18e6638b95dd01
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-10 03:05:02 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// a script that imports an array and computes a new array.
 const data = require('./100-data').list;
 console.log(data);
 console.log(data.map((e, idx) => e * idx));
