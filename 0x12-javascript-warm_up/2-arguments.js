7a48d0c682eae576920f2ced28fdcc1383cb6067
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-11 05:24:30 +0200

    T2 remake

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-// Print a message depending on number of arguments passed
+// a script that prints a message depending of the number of arguments passed:
 
+// If no arguments are passed to the script, print “No argument”
+// If only one argument is passed to the script, print “Argument found”
+// Otherwise, print “Arguments found”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
 const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
-} else if (args.length === 3) {
+
+if (args[3]) {
+  console.log('Arguments found');
+} else if (args[2]) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
