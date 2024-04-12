9a6a94290c69e4614204f85f9d4f24c3a73a5f6d
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-12 17:36:50 +0200

    T8 remkae

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,24 @@
 #!/usr/bin/node
-const Number = Math.floor(+process.argv[2]);
-let i = Number;
+// a script that prints a square
 
-while (i > 0) {
-  console.log('X'.repeat(Number));
-  i--;
+// The first argument is the size of the square
+// If the first argument can’t be converted to an integer, print “Missing size”
+// You must use the character X to print the square
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You must use a loop (while, for, etc.)
+
+const printable = 'X';
+if (isNaN(parseInt(process.argv[2], 10))) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(process.argv[2], 10);
+
+  for (let index = 0; index < size; index++) {
+    let rowContent = '';
+    for (let index1 = 0; index1 < size; index1++) {
+      rowContent += printable;
+    }
+    console.log(rowContent);
+  }
 }
-isNaN(Number) && console.log('Missing size');
