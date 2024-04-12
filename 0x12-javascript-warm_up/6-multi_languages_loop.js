fc855539dfb95ca6b6b27fc920fc39d2058720bc
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-12 17:07:14 +0200

    T6 remake

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,20 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop for this task
+// a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
-const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use any if/else statement
+// You can use only one console.log
+// You must use a loop (while, for, etc.)
 
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+const first = 'C is fun';
+const second = 'Python is cool';
+const last = 'JavaScript is amazing';
+const langarray = [first, second, last];
+for (let index = 0; index < langarray.length; index++) {
+  const element = langarray[index];
+  console.log(element);
 }
