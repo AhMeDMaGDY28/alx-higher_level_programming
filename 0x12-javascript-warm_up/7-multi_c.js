af816241d9fd8f952c40cc93ccaae553cd6212e8
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-12 17:23:51 +0200

    T7 remake

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
-// print lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
+// a script that prints x times “C is fun”
 
-const lang = 'C is fun';
+// Where x is the first argument of the script
+// If the first argument can’t be converted to an integer, print “Missing number of occurrences”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You can use only two console.log
+// You must use a loop (while, for, etc.)
 
-if (isNaN(process.argv[2])) {
+const printable = 'C is fun';
+if (isNaN(parseInt(process.argv[2], 10))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(lang);
+  for (let index = 0; index < parseInt(process.argv[2], 10); index++) {
+    console.log(printable);
   }
 }
