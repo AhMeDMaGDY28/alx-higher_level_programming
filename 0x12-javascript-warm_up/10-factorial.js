b5d1b1c2c164b05aa995bb6bccc6b6c2c0642ae8
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-13 04:02:31 +0200

    T10 remake 1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-
-// The first argument is integer (argument can be cast as integer) used for computing the factorial
-// Factorial of NaN is 1
-// You must do it recursively
-// You must use a function
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-
-function factorial(num) {
-  if (num === 1 || isNaN(num)) {
-    return 1;
-  }
-  return num * factorial(num - 1);
+const Number = Math.floor(+process.argv[2]);
+function rec(num) {
+  return num ? num * rec(num - 1) : 1;
 }
-
-console.log(factorial(parseInt(process.argv[2])));
+console.log(rec(Number));
