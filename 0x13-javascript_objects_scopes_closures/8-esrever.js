19795792c6e42c62689023c03817e97d29381f82
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-15 03:25:30 +0200

    T8 remake

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
+
 // a function that returns the reversed version of a list:
+// Prototype: exports.esrever = function (list)
+// You are not allow to use the built-in method reverse
+
 exports.esrever = function (list) {
-  const newArray = [];
-  list.map(e => newArray.unshift(e));
-  return newArray;
+  const NewList = [];
+  let NewIndex = 0;
+  for (let OldIndex = list.length - 1; OldIndex >= 0; OldIndex--) {
+    NewList[NewIndex] = list[OldIndex];
+    NewIndex++;
+  }
+  return NewList;
 };
