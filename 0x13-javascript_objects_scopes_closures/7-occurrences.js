797a382b27f2871eb61689e4086af3c883b1a1a3
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-15 03:07:04 +0200

    T7 remake 1

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 // a function that returns the number of occurrences in a list:
 // Prototype: exports.nbOccurences = function (list, searchElement)
 
-exports.nbOccurences = function(list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let num = 0;
   for (let index = 0; index < list.length; index++) {
     const element = list[index];
