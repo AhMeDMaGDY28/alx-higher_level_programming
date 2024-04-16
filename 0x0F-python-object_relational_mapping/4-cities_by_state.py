c40d0de25fba449880a3c5a6c60f8f0d52dff969
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-17 01:34:00 +0200

    T4 remodel

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python3
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
 
-"""a script that lists all cities from the database hbtn_0e_4_usa"""
-
+Your script should take 3 arguments: mysql username, mysql password and database name
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+You can use only execute() once
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+# to use the argv from the system when running the code
 from sys import argv
+
+# to use database with python
 import MySQLdb
 
 
+# this stop the code from excuting if imported
 if __name__ == "__main__":
-    database_connect = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+    # Connect to the database
+    Data_Base_connection = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
-    cursor = database_connect.cursor()
-    cursor.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities,\
-                states WHERE cities.state_id = states.id ORDER BY\
-                cities.id"
-    )
-    rows = cursor.fetchall()
-    for row in rows:
-        print(f"{row}")
+
+    # making a cursor using the connection
+    cursor = Data_Base_connection.cursor()
+
+    # the sql formula which going to be excuted
+    Sql_Formula = "\
+    SELECT cities.id, cities.name, states.name FROM cities, states\
+    WHERE state_id = states.id\
+    ORDER BY cities.id"
+
+    # the excute command to excute the formula
+    cursor.execute(Sql_Formula)
+
+    # fecthing all the data
+    DATA_fetched_Rows = cursor.fetchall()
+
+    # printing the data
+    for data_row in DATA_fetched_Rows:
+        print(data_row)
+
+    # closing the cursor
+    cursor.close()
+
+    # closing the connection
+    Data_Base_connection.close()
