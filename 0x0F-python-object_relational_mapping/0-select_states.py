d219ec79cd284bffb3b26b7dc093f60ee1130dfb
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-16 03:45:06 +0200

    T1 fix 3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -19,12 +19,7 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cursor = db_connection.cursor()
-    cursor.execute(
-        """
-                SELECT * FROM states
-                ORDER BY states.id
-                """
-    )
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
     rows = cursor.fetchall()
     for row in rows:
         print(f"{row}")
