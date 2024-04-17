e74d4f8ea25ae5bf74d9d7972557433331658c76
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-17 07:19:51 +0200

    T14 remodel

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,41 +1,62 @@
 #!/usr/bin/python3
-
 """
-A script to print all cities with their respective states.
+a script 14-model_city_fetch_by_state.py that prints all
+City objects from the database hbtn_0e_14_usa:
 
-Usage:
-    ./print_cities.py <username> <password> <database>
-
-Arguments:
-    username: The username to connect to the MySQL database.
-    password: The password to connect to the MySQL database.
-    database: The name of the MySQL database.
-
-Example:
-    ./print_cities.py root password hbtn_0e_14_usa
-"""
-
-import sys
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
 from model_state import Base, State
-from model_city import City
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+Results must be display as they are in the example below
+(<state name>: (<city id>) <city name>)
+Your code should not be executed when imported
+"""
+
+# Import necessary modules
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from model_city import City
 
-
+# this stop the code from excuting if imported
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
+    # Construct the database connection string
+    DataBase_connection_Formula = (
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
     )
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(DataBase_connection_Formula, pool_pre_ping=True)
+
+    # Create all tables in the database (if they do not exist already)
     Base.metadata.create_all(engine)
+
+    # Create a sessionmaker instance
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
 
-    """Query all cities with their respective states and print the results"""
-    for state, city in (
-        session.query(State, City).filter(State.id == City.state_id).
-        order_by(City.id)
-    ):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    try:
+        # Retrieve all State objects and print them
+
+        states_id_city = (
+            session.query(State, City)
+            .filter(State.id == City.state_id)
+            .order_by(City.id)
+            .all()
+        )
+
+        for state, city in states_id_city:
+            print(f"{state.name}: ({city.id}) {city.name}")
+
+    finally:
+        # Close the session
+        session.close()
+
+    # Dispose of the engine
+    engine.dispose()
