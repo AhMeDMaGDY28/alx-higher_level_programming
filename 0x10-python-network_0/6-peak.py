6d146cd56bffc38e907c8347a7a205d46591a2f7
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-27 03:52:32 +0300

    all

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""finds a peak in a list of integers"""
+
+
+def find_peak(lst):
+    """a Function that finds a peak in a list of integers"""
+    length = len(lst)
+    if length == 0:
+        return None
+
+    def peak(length, m):
+        """ " Help function to find the peak"""
+        if (m == 0 or lst[m] >= lst[m - 1]) and (
+            m == length - 1 or lst[m] >= lst[m + 1]
+        ):
+            return lst[m]
+        if m > 0 and lst[m - 1] > lst[m]:
+            return peak(m, m // 2)
+        return peak(length, m + (length - m) // 2)
+
+    return peak(length, length // 2)
