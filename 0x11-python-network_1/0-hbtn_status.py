0125dd40caf80ce66a1ba270975b75ae0a88399d
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-29 02:36:05 +0300

    2024-04-29 02:36:05 AM

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
 
+import urllib
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status") as response:
         html_file = response.read()
         print("Body response:")
         print("\t- type:", type(html_file))
