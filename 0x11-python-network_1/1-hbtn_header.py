52e8dabda59d9f65119de2400ffae756133b9be7
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-29 02:50:06 +0300

    2024-04-29 02:50:06 AM

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,3 +2,15 @@
 """a Python script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id
 variable found in the header of the response."""
+
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        id_head_req = response.headers.get("X-Request-Id")
+        print(id_head_req)
