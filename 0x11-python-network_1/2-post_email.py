68d5060aacf2b592454b68420ce5c62f27c64123
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-04-29 04:05:40 +0300

    2024-04-29 04:05:40 AM

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,17 +7,14 @@ from sys import argv
 import urllib.parse
 import urllib.request
 
+
 if __name__ == "__main__":
-    url = argv[1]
+    URL = argv[1]
     email = argv[2]
 
-    val = {"email": email}
-    # Encode the email parameter
-    data = urllib.parse.urlencode(val)
-    data = data.encode("utf-8")  # Data should be bytes
-    req = urllib.request.Request(url, data)
-
-    with urllib.request.urlopen(req) as respon:
-        # Read the response body and decode it
-        resp_body = respon.read()
-        print(resp_body)
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode("utf-8")
+    request = urllib.request.Request(URL, DATA)
+    with urllib.request.urlopen(request) as respon:
+        body_resp = respon.read()
+        print(body_resp.decode("utf-8"))
