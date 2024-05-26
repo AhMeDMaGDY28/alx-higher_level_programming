b1a243f2fae5f36fbf8aa76bdda94539bf5b1d4a
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-27 02:56:07 +0300

    2024-05-27 02:56:07 AM

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { argv } = require('process');
+const request = require('request');
+
+const id = argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  try {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
